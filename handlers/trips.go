25d308e89c38e096e3c7293c66912909b074a6fa
PathakPratik <rajpratik10@gmail.com>
2021-11-27 01:52:52 +0000

    added decorator for trips res

--- handlers/trips.go
+++ handlers/trips.go
@@ -3,9 +3,34 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"sort"
 
 	"github.com/TheCyclistGoServer/AwsDynamoDb"
 )
+
+func decorator(trips []AwsDynamoDb.TrackingData) [][]AwsDynamoDb.TrackingData {
+	sort.Slice(trips, func(i, j int) bool {
+		return trips[i].TripId > trips[j].TripId
+	})
+
+	var tripPoints [][]AwsDynamoDb.TrackingData
+	var res []AwsDynamoDb.TrackingData
+	currTrip := trips[0].TripId
+	for _, trip := range trips {
+		if trip.TripId != currTrip {
+			tripPoints = append(tripPoints, res)
+			res = []AwsDynamoDb.TrackingData{}
+			currTrip = trip.TripId
+		}
+
+		res = append(res, AwsDynamoDb.TrackingData{
+			TripId: trip.TripId, UserId: trip.UserId, RecordId: trip.RecordId, Latitude: trip.Latitude, Longitude: trip.Longitude, Timestamp: trip.Timestamp,
+		})
+	}
+	tripPoints = append(tripPoints, res)
+
+	return tripPoints
+}
 
 func TripsHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -25,7 +50,7 @@ func TripsHandler(w http.ResponseWriter, r *http.Request) {
 
 	if success {
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(trips)
+		json.NewEncoder(w).Encode(decorator(trips))
 	} else {
 		http.Error(w, "Invalid Query", http.StatusBadRequest)
 	}
